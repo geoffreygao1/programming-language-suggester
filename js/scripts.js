d88dca68a78a2656ed591522f4c53ecbd1b9c4e7
Geoffrey Gao <geoffreygao1@gmail.com>
2022-11-03 12:08:29 -0700

    added question hiding functionality

--- js/scripts.js
+++ js/scripts.js
@@ -12,15 +12,58 @@ function tallyScore(event) {
 
   // document.getElementById("output").innerText = result;
 }
-//nextButton will hide the current div and unhide the next div
 
+//nextButton will hide the current div and unhide the next div
+function nextButton(id) {
+  let intro = document.getElementById("intro");
+  let start = document.getElementById("start");
+  let q1 = document.getElementById("question1");
+  let q2 = document.getElementById("question2");
+  let q3 = document.getElementById("question3");
+  let q4 = document.getElementById("question4");
+  let q5 = document.getElementById("question5");
+  let result = document.getElementById("result");
+
+  if (id === "start") {
+    start.setAttribute("class", "hidden");
+    intro.setAttribute("class", "hidden");
+    q1.removeAttribute("class");
+  } else if (id === "q1") {
+    q1.setAttribute("class", "hidden");
+    q2.removeAttribute("class");
+  } else if (id === "q2") {
+    q2.setAttribute("class", "hidden");
+    q3.removeAttribute("class");
+  } else if (id === "q3") {
+    q3.setAttribute("class", "hidden");
+    q4.removeAttribute("class");
+  } else if (id === "q4") {
+    q4.setAttribute("class", "hidden");
+    q5.removeAttribute("class");
+  } else if (id === "q5") {
+    q5.setAttribute("class", "hidden");
+    result.removeAttribute("class");
+  }
+}
 
 //UI Logic
 window.addEventListener("load", function () {
   const form = document.getElementById("survey");
+  const startBtn = document.getElementById("startBtn");
+  const q1Btn = document.getElementById("q1Btn");
+  const q2Btn = document.getElementById("q2Btn");
+  const q3Btn = document.getElementById("q3Btn");
+  const q4Btn = document.getElementById("q4Btn");
   const submitBtn = document.getElementById("submitBtn");
+
   //initialize hidden sections
 
   //listen for submit button
+  startBtn.addEventListener("click", function () { nextButton("start") });
+  q1Btn.addEventListener("click", function () { nextButton("q1") });
+  q2Btn.addEventListener("click", function () { nextButton("q2") });
+  q3Btn.addEventListener("click", function () { nextButton("q3") });
+  q4Btn.addEventListener("click", function () { nextButton("q4") });
   form.addEventListener("submit", tallyScore);
+  submitBtn.addEventListener("click", function () { nextButton("q5") });
 });
